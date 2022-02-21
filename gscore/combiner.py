ef440a219012d78a25305014db3736024f3fa720
Aaron Scott <aaron.scott@med.lu.se>
2022-02-21 23:29:19 +0100

    Fixed issue with the combiner function, the export now uses the correct q-value fields

--- gscore/combiner.py
+++ gscore/combiner.py
@@ -124,7 +124,7 @@ class PrecursorExport(MutableMapping):
 
                 if sample in record:
 
-                    if record[sample].probability > 0.5:
+                    if record[sample].q_value <= self.max_q_value:
 
                         export_record[sample] = record.get_sample_intensity(
                             sample_key=sample
