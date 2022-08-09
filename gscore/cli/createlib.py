99e53bf4c893b54fedb6f818d327dd2b6a2ee772
Aaron Scott <aaron.scott@med.lu.se>
2022-08-09 21:54:03 +0200

    Added command line functionality to combine multiple predicted peakgroup files and create a spectral library from these files.

---
+++ gscore/cli/createlib.py
@@ -0,0 +1,223 @@
+import argparse
+import csv
+
+from typing import Any
+
+from gscore.parsers.pqp_file import PQPFile
+
+
+def combine_predicted_peakgroups(predicted_peakgroup_files):
+    predicted_peakgroups = dict()
+
+    for predicted_peakgroup_file in predicted_peakgroup_files:
+
+        with open(predicted_peakgroup_file) as predicted_file_h:
+
+            reader = csv.DictReader(predicted_file_h, delimiter="\t")
+
+            for row in reader:
+
+                if row["Decoy"] == "0" and row["PeakgroupPrediction"] == "1.0":
+
+                    record = {
+                        "UnmodifiedSequence": row["UnmodifiedSequence"],
+                        "ModifiedSequence": row["ModifiedSequence"],
+                        "Charge": int(row["Charge"]),
+                        "Protein": row["Protein"],
+                        "Decoy": int(row["Decoy"]),
+                        "RT": float(row["RT"])
+                    }
+
+                    precursor_id = f"{row['ModifiedSequence']}_{row['Charge']}"
+
+                    if precursor_id not in predicted_peakgroups:
+                        predicted_peakgroups[precursor_id] = record
+
+    return list(predicted_peakgroups.values())
+
+
+def create_new_library(new_library_path, library_annotated_transitions, used_precursors):
+    protein_records = dict()
+    peptide_protein_mapping = dict()
+    peptide_records = dict()
+    precursor_peptide_mapping = dict()
+    precursor_records = dict()
+    transition_precursor_mapping = dict()
+    transition_records = dict()
+
+    for transition in library_annotated_transitions:
+
+        precursor_id = f"{transition['MODIFIED_SEQUENCE']}_{transition['CHARGE']}"
+
+        if precursor_id in used_precursors:
+
+            if transition["PROTEIN_ACCESSION"] not in protein_records:
+                protein_records[transition["PROTEIN_ACCESSION"]] = {
+                    "ID": transition["PROTEIN_ID"],
+                    "PROTEIN_ACCESSION": transition["PROTEIN_ACCESSION"],
+                    "DECOY": transition["DECOY"]
+                }
+
+            peptide_protein_id = f"{transition['PEPTIDE_ID']}_{transition['PROTEIN_ID']}"
+
+            if peptide_protein_id not in peptide_protein_mapping:
+                peptide_protein_mapping[peptide_protein_id] = {
+                    "PEPTIDE_ID": transition["PEPTIDE_ID"],
+                    "PROTEIN_ID": transition["PROTEIN_ID"]
+                }
+
+            if transition["MODIFIED_SEQUENCE"] not in peptide_records:
+                peptide_records[transition["MODIFIED_SEQUENCE"]] = {
+                    "ID": transition["PEPTIDE_ID"],
+                    "UNMODIFIED_SEQUENCE": transition["UNMODIFIED_SEQUENCE"],
+                    "MODIFIED_SEQUENCE": transition["MODIFIED_SEQUENCE"],
+                    "DECOY": transition["DECOY"]
+                }
+
+            precursor_peptide_id = f"{transition['PRECURSOR_ID']}_{transition['PEPTIDE_ID']}"
+
+            if precursor_peptide_id not in precursor_peptide_mapping:
+                precursor_peptide_mapping[precursor_peptide_id] = {
+                    "PRECURSOR_ID": transition["PRECURSOR_ID"],
+                    "PEPTIDE_ID": transition["PEPTIDE_ID"]
+                }
+
+            if precursor_id not in precursor_records:
+                precursor_records[precursor_id] = {
+                    "ID": transition["PRECURSOR_ID"],
+                    "TRAML_ID": transition["PRECURSOR_TRAML_ID"],
+                    "PRECURSOR_MZ": transition["PRECURSOR_MZ"],
+                    "CHARGE": transition["PRECURSOR_CHARGE"],
+                    "LIBRARY_RT": transition["LIBRARY_RT"],
+                    "DECOY": transition["DECOY"]
+                }
+
+            transition_precursor_id = f"{transition['TRANSITION_ID']}_{transition['PRECURSOR_ID']}"
+
+            if transition_precursor_id not in transition_precursor_mapping:
+                transition_precursor_mapping[transition_precursor_id] = {
+                    "TRANSITION_ID": transition["TRANSITION_ID"],
+                    "PRECURSOR_ID": transition["PRECURSOR_ID"]
+                }
+
+            if transition["TRANSITION_ID"] not in transition_records:
+                transition_records[transition["TRANSITION_ID"]] = {
+                    "ID": transition["TRANSITION_ID"],
+                    "TRAML_ID": transition["TRANSITION_TRAML_ID"],
+                    "PRODUCT_MZ": transition["PRODUCT_MZ"],
+                    "CHARGE": transition["TRANSITION_CHARGE"],
+                    "TYPE": transition["TYPE"],
+                    "ANNOTATION": transition["ANNOTATION"],
+                    "ORDINAL": transition["ORDINAL"],
+                    "DETECTING": transition["DETECTING"],
+                    "IDENTIFYING": transition["IDENTIFYING"],
+                    "QUANTIFYING": transition["QUANTIFYING"],
+                    "LIBRARY_INTENSITY": transition["LIBRARY_INTENSITY"],
+                    "DECOY": transition["DECOY"]
+                }
+
+    protein_records = protein_records.values()
+    peptide_protein_mapping = peptide_protein_mapping.values()
+    peptide_records = peptide_records.values()
+    precursor_peptide_mapping = precursor_peptide_mapping.values()
+    precursor_records = precursor_records.values()
+    transition_precursor_mapping = transition_precursor_mapping.values()
+    transition_records = transition_records.values()
+
+    new_library = PQPFile(new_library_path)
+    new_library.create_tables()
+
+    gene_records = [
+        {
+            "gene_name": "NA",
+            "decoy": 0
+        }
+    ]
+
+    version_record = [
+        {
+            "id": 3
+        }
+    ]
+
+    new_library.add_records("VERSION", version_record)
+    new_library.add_records("GENE", gene_records)
+
+    peptide_gene_map_ids = []
+
+    added_genes = new_library.get_genes()
+
+    gene_id = added_genes[0]["ID"]
+
+    for peptide_protein_mapping_record in peptide_protein_mapping:
+        peptide_gene_map_ids.append(
+            {
+                "peptide_id": peptide_protein_mapping_record["PEPTIDE_ID"],
+                "gene_id": gene_id
+            }
+        )
+
+    new_library.add_records("PEPTIDE_GENE_MAPPING", peptide_gene_map_ids)
+
+    new_library.add_records("PROTEIN", protein_records)
+    new_library.add_records("PEPTIDE", peptide_records)
+    new_library.add_records("PRECURSOR", precursor_records)
+    new_library.add_records("TRANSITION", transition_records)
+    new_library.add_records("PEPTIDE_PROTEIN_MAPPING", peptide_protein_mapping)
+    new_library.add_records("PRECURSOR_PEPTIDE_MAPPING", precursor_peptide_mapping)
+    new_library.add_records("TRANSITION_PRECURSOR_MAPPING", transition_precursor_mapping)
+
+
+class CreateLib:
+    name: str
+    parser: argparse.ArgumentParser
+
+    def __init__(self) -> None:
+        self.name = "createlib"
+
+    def __call__(self, args: argparse.Namespace) -> None:
+        predicted_peakgroups = combine_predicted_peakgroups(args.input_files)
+
+        print(predicted_peakgroups[0])
+
+        print("Parsing spectral library.")
+
+        pqp_file = PQPFile(args.pqp)
+
+        library_annotated_transitions = pqp_file.get_annotated_transitions()
+
+        used_precursors = set(
+            [f"{row['ModifiedSequence']}_{row['Charge']}" for row in predicted_peakgroups]
+        )
+
+        create_new_library(
+            args.output,
+            library_annotated_transitions,
+            used_precursors
+        )
+
+    def build_subparser(self, subparser: Any) -> None:
+        self.parser = subparser.add_parser(
+            self.name,
+            help="Combine multiple predicted runs into PQP spectral library.",
+        )
+
+        self.parser.add_argument(
+            "--input-files",
+            dest="input_files",
+            nargs="+",
+            help="List of files to compile",
+        )
+
+        self.parser.add_argument("-o", "--output", dest="output", help="Output file spectral library")
+
+        self.parser.add_argument(
+            "--pqp",
+            dest="pqp",
+            help="Base spectral library to extract fragments from"
+        )
+
+        self.parser.set_defaults(run=self)
+
+    def __repr__(self) -> str:
+        return f"<Export> {self.name}"
---
+++ gscore/parsers/pqp_file.py
@@ -0,0 +1,507 @@
+import sqlite3
+
+
+CREATE_SCHEMA = [
+"""
+CREATE TABLE IF NOT EXISTS VERSION(
+    ID INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS GENE(
+    ID INTEGER PRIMARY KEY,
+    GENE_NAME TEXT NOT NULL,
+    DECOY INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS PEPTIDE_GENE_MAPPING(
+    PEPTIDE_ID INT NOT NULL,
+    GENE_ID INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS PROTEIN(
+    ID INTEGER PRIMARY KEY,
+    PROTEIN_ACCESSION TEXT NOT NULL,
+    DECOY INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS PEPTIDE_PROTEIN_MAPPING(
+    PEPTIDE_ID INT NOT NULL,
+    PROTEIN_ID INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS PEPTIDE(
+    ID INTEGER PRIMARY KEY,
+    UNMODIFIED_SEQUENCE TEXT NOT NULL,
+    MODIFIED_SEQUENCE TEXT NOT NULL,
+    DECOY INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS PRECURSOR_PEPTIDE_MAPPING(
+    PRECURSOR_ID INT NOT NULL,
+    PEPTIDE_ID INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS COMPOUND(
+    ID INTEGER PRIMARY KEY,
+    COMPOUND_NAME TEXT NOT NULL,
+    SUM_FORMULA TEXT NOT NULL,
+    SMILES TEXT NOT NULL,
+    ADDUCTS TEXT NOT NULL,
+    DECOY INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS PRECURSOR_COMPOUND_MAPPING(
+    PRECURSOR_ID INT NOT NULL,
+    COMPOUND_ID INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS PRECURSOR(
+    ID INTEGER PRIMARY KEY,
+    TRAML_ID TEXT NULL,
+    GROUP_LABEL TEXT NULL,
+    PRECURSOR_MZ REAL NOT NULL,
+    CHARGE INT NULL,
+    LIBRARY_INTENSITY REAL NULL,
+    LIBRARY_RT REAL NULL,
+    LIBRARY_DRIFT_TIME REAL NULL,
+    DECOY INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS TRANSITION_PRECURSOR_MAPPING(
+    TRANSITION_ID INT NOT NULL,
+    PRECURSOR_ID INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS TRANSITION_PEPTIDE_MAPPING(
+    TRANSITION_ID INT NOT NULL,
+    PEPTIDE_ID INT NOT NULL
+);""",
+
+"""CREATE TABLE IF NOT EXISTS TRANSITION(
+    ID INTEGER PRIMARY KEY,
+    TRAML_ID TEXT NULL,
+    PRODUCT_MZ REAL NOT NULL,
+    CHARGE INT NULL,
+    TYPE CHAR(255) NULL,
+    ANNOTATION TEXT NULL,
+    ORDINAL INT NULL,
+    DETECTING INT NOT NULL,
+    IDENTIFYING INT NOT NULL,
+    QUANTIFYING INT NOT NULL,
+    LIBRARY_INTENSITY REAL NULL,
+    DECOY INT NOT NULL
+);"""
+]
+class PQPFile:
+
+    ADD_RECORD = """INSERT INTO {table_name} ({list_fields}) VALUES ({list_values});"""
+
+    UPDATE_RECORD = (
+        """UPDATE {table_name} SET {update_values} WHERE {key_field} = {record_id};"""
+    )
+
+    CREATE_INDEX = (
+        """CREATE INDEX IF NOT EXISTS {index_name} ON {table_name}({column_list});"""
+    )
+
+    def __init__(self, file_path: str = ""):
+
+        self.db_path = file_path
+
+    def __enter__(self):
+
+        self.conn = sqlite3.connect(self.db_path)
+
+        self.conn.row_factory = sqlite3.Row
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+
+        self.conn.close()
+
+    def create_tables(self):
+
+        for create_table_query in CREATE_SCHEMA:
+
+            self.create_table(create_table_query)
+
+    def get_genes(self):
+
+        gene_query = """
+        SELECT
+            *
+        FROM GENE;
+        """
+
+        genes = []
+
+        for gene in self.iterate_records(gene_query):
+
+            genes.append(gene)
+
+        return genes
+
+    def get_proteins(self):
+
+        protein_query = """
+        SELECT
+            *
+        FROM PROTEIN;
+        """
+
+        proteins = []
+
+        for protein in self.iterate_records(protein_query):
+
+            proteins.append(protein)
+
+        return proteins
+
+    def get_peptides(self):
+
+        peptide_query = """
+        SELECT
+            *
+        FROM PEPTIDE;
+        """
+
+        peptides = []
+
+        for peptide in self.iterate_records(peptide_query):
+
+            peptides.append(peptide)
+
+        return peptides
+
+    def get_precursors(self):
+
+        precursor_query = """
+        SELECT
+            *
+        FROM PRECURSOR;
+        """
+
+        precursors = []
+
+        for precursor in self.iterate_records(precursor_query):
+
+            precursors.append(precursor)
+
+        return precursors
+
+    def get_annotated_precursors(self):
+
+        query = """
+        SELECT
+            *
+        FROM PRECURSOR
+        JOIN PRECURSOR_PEPTIDE_MAPPING on PRECURSOR.ID = PRECURSOR_PEPTIDE_MAPPING.PRECURSOR_ID
+        JOIN PEPTIDE on PRECURSOR_PEPTIDE_MAPPING.PEPTIDE_ID = PEPTIDE.ID
+        JOIN PEPTIDE_PROTEIN_MAPPING on PEPTIDE.ID = PEPTIDE_PROTEIN_MAPPING.PEPTIDE_ID
+        JOIN PROTEIN on PEPTIDE_PROTEIN_MAPPING.PROTEIN_ID = PROTEIN.ID;
+        """
+
+        records = []
+
+        for record in self.iterate_records(query):
+
+            records.append(record)
+
+        return records
+
+    def get_annotated_transitions(self):
+
+        query = """
+        SELECT
+            TRANSITION.TRAML_ID TRANSITION_TRAML_ID,
+            PRECURSOR.TRAML_ID PRECURSOR_TRAML_ID,
+            PRECURSOR.CHARGE PRECURSOR_CHARGE,
+            TRANSITION.CHARGE TRANSITION_CHARGE,
+            *
+        FROM TRANSITION
+        JOIN TRANSITION_PRECURSOR_MAPPING on TRANSITION.ID = TRANSITION_PRECURSOR_MAPPING.TRANSITION_ID
+        JOIN PRECURSOR on TRANSITION_PRECURSOR_MAPPING.PRECURSOR_ID = PRECURSOR.ID
+        JOIN PRECURSOR_PEPTIDE_MAPPING on PRECURSOR.ID = PRECURSOR_PEPTIDE_MAPPING.PRECURSOR_ID
+        JOIN PEPTIDE on PRECURSOR_PEPTIDE_MAPPING.PEPTIDE_ID = PEPTIDE.ID
+        JOIN PEPTIDE_PROTEIN_MAPPING on PEPTIDE.ID = PEPTIDE_PROTEIN_MAPPING.PEPTIDE_ID
+        JOIN PROTEIN on PEPTIDE_PROTEIN_MAPPING.PROTEIN_ID = PROTEIN.ID;
+        """
+
+        records = []
+
+        for record in self.iterate_records(query):
+            records.append(record)
+
+        return records
+
+
+    def get_transitions(self):
+
+        transition_query = """
+        SELECT
+            *
+        FROM TRANSITION;
+        """
+
+        transitions = []
+
+        for transition in self.iterate_records(transition_query):
+
+            transitions.append(transition)
+
+        return transitions
+
+    def get_precursor_peptide_mapping(self):
+
+        query = """
+        SELECT
+            *
+        FROM PRECURSOR_PEPTIDE_MAPPING;
+        """
+
+        records = []
+
+        for record in self.iterate_records(query):
+
+            records.append(record)
+
+        return records
+
+    def get_peptide_protein_mapping(self):
+
+        query = """
+        SELECT
+            *
+        FROM PEPTIDE_PROTEIN_MAPPING;
+        """
+
+        records = []
+
+        for record in self.iterate_records(query):
+            records.append(record)
+
+        return records
+
+    def get_transition_precursor_mapping(self):
+
+        query = """
+        SELECT
+            *
+        FROM TRANSITION_PRECURSOR_MAPPING;
+        """
+
+        records = []
+
+        for record in self.iterate_records(query):
+            records.append(record)
+
+        return records
+
+    def iterate_records(self, query: str):
+
+        with self:
+
+            cursor = self.conn.cursor()
+
+            cursor.execute(query)
+
+            for row in cursor:
+
+                if len(row) > 0:
+
+                    record = {column: value for column, value in zip(row.keys(), row)}
+
+                    yield record
+
+    def add_records(self, table_name="", records=[]):
+
+        with self:
+
+            input_records = list()
+
+            for record_num, record in enumerate(records):
+
+                field_names = list()
+                field_values = list()
+
+                for field_name, field_value in record.items():
+                    field_names.append(field_name)
+
+                    field_values.append(field_value)
+
+                input_records.append(tuple(field_values))
+
+                if record_num % 1000 == 0 and record_num > 0:
+                    holders = ",".join("?" * len(field_names))
+
+                    add_record_query = self.ADD_RECORD.format(
+                        table_name=table_name,
+                        list_fields=",".join(field_names),
+                        list_values=holders,
+                    )
+
+                    cursor = self.conn.cursor()
+
+                    cursor.executemany(add_record_query, input_records)
+
+                    self.conn.commit()
+
+                    input_records = list()
+
+            if input_records:
+                holders = ",".join("?" * len(field_names))
+
+                add_record_query = self.ADD_RECORD.format(
+                    table_name=table_name,
+                    list_fields=",".join(field_names),
+                    list_values=holders,
+                )
+
+                cursor = self.conn.cursor()
+
+                cursor.executemany(add_record_query, input_records)
+
+                self.conn.commit()
+
+                input_records = list()
+
+    def update_records(self, table_name="", key_field="", records={}):
+
+        """
+        records = dict()
+
+        Key in records is the record id
+        """
+
+        index_query = self.CREATE_INDEX.format(
+            index_name=f"idx_{key_field}_{table_name}",
+            table_name=table_name,
+            column_list=key_field,
+        )
+
+        self.run_raw_sql(index_query)
+
+        input_records = list()
+
+        for record_num, (record_id, record) in enumerate(records.items()):
+
+            field_values = list()
+
+            field_names = list()
+
+            for field_name, field_value in record.items():
+                field_values.append(field_value)
+
+                field_names.append(field_name)
+
+            field_values.append(record_id)
+
+            input_records.append(field_values)
+
+            if record_num % 1000 == 0 and record_num > 0:
+
+                if len(field_names) == 1:
+
+                    formatted_update_string_holder = f"{field_names[0]}=?"
+
+                else:
+
+                    formatted_field_names = [
+                        f"{field_name}=?" for field_name in field_names
+                    ]
+
+                    formatted_update_string_holder = ", ".join(formatted_field_names)
+
+                update_record_query = self.UPDATE_RECORD.format(
+                    table_name=table_name,
+                    update_values=formatted_update_string_holder,
+                    key_field=key_field,
+                    record_id="?",
+                )
+
+                cursor = self.conn.cursor()
+
+                try:
+
+                    cursor.executemany(update_record_query, input_records)
+
+                except sqlite3.OperationalError as e:
+
+                    print(update_record_query)
+                    # print(input_records)
+                    raise e
+
+                self.conn.commit()
+
+                input_records = list()
+
+        if input_records:
+
+            if len(field_names) == 1:
+
+                formatted_update_string_holder = f"{field_names[0]}=?"
+
+            else:
+
+                formatted_field_names = [
+                    f"{field_name}=?" for field_name in field_names
+                ]
+
+                formatted_update_string_holder = ", ".join(formatted_field_names)
+
+            update_record_query = self.UPDATE_RECORD.format(
+                table_name=table_name,
+                update_values=formatted_update_string_holder,
+                key_field=key_field,
+                record_id="?",
+            )
+
+            cursor = self.conn.cursor()
+
+            try:
+
+                cursor.executemany(update_record_query, input_records)
+
+            except sqlite3.OperationalError as e:
+
+                print(update_record_query)
+                print(input_records)
+                raise e
+
+            self.conn.commit()
+
+    def run_raw_sql(self, sql):
+
+        with self:
+
+            cursor = self.conn.cursor()
+
+            cursor.execute(sql)
+
+    def create_table(self, query):
+
+        self.run_raw_sql(query)
+
+    def drop_table(self, table_name):
+
+        query = f"drop table if exists {table_name};"
+
+        cursor = self.conn.cursor()
+
+        cursor.execute(query)
+
+    def __contains__(self, table_name: str) -> bool:
+
+        query = (
+            """SELECT name FROM sqlite_master "
+            f"WHERE type='table' AND name='{table_name}';"""
+        ).format(table_name=table_name)
+
+        cursor = self.conn.cursor()
+
+        cursor.execute(query)
+
+        result = cursor.fetchone()
+
+        return bool(result)
